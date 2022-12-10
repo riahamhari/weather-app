32ccb1fed6a54d3cebcb7345c190013819381915
riahamhari <109351021+riahamhari@users.noreply.github.com>
2022-12-10 00:51:24 +0000

    code fixes

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import { clearDisplay } from "./modules/display";
 
 const btn = document.querySelector('button')
 const input = document.querySelector('input')
+const API_KEY = config.API_KEY;
 
 async function getWeather() {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric&appid=3df8a87758f9170b7460a19f89b1958b`, { mode: 'cors' });
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric&appid=${API_KEY}`, { mode: 'cors' });
     const weatherData = await response.json();
-    console.log(weatherData)
+
     const newWeather = processWeather(weatherData)
     displayInfo(newWeather)
 
